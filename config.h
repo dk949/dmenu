392d6abb0267beade127991c19286e88183cfda8
dk949 <david2000katz@gmail.com>
2020-09-13 23:11:18 +0300

    Everything must be transparant

--- config.h
+++ config.h
@@ -2,6 +2,8 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static double opacity = 1.0;                /* -o  option; defines alpha translucency        */
+
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 1500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -9,6 +11,16 @@ static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+};
+
+
 static const char *colors[SchemeLast][2] = {
 	/*                              fg         bg       */
 	[SchemeNorm]             = { "#E6E6D1", "#101421" }, // dull white    / dark purple
