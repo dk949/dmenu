9f329099ae30324c92df53d8b14ff8d95bccf203
dk949 <56653556+dk949@users.noreply.github.com>
2022-06-08 22:44:07 +0300

    removed (some) order dependent includes

--- drw.c
+++ drw.c
@@ -1,12 +1,7 @@
 /* See LICENSE file for copyright and license details. */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <X11/Xlib.h>
-#include <X11/Xft/Xft.h>
-
 #include "drw.h"
 #include "util.h"
+
 
 #define UTF_INVALID 0xFFFD
 #define UTF_SIZ     4
