3f33897c7444f8c9affe079a631f8e887101a6fd
dk949 <56653556+dk949@users.noreply.github.com>
2023-05-27 01:15:26 +0300

    simplified config

--- config.mk
+++ config.mk
@@ -2,33 +2,26 @@
 VERSION = $(shell git describe)
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+DESTDIR   ?=
+PREFIX    ?= /usr/local
+MANPREFIX ?= $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+REQ_LIBS = x11 fontconfig xft xrender
+
+X11FLAGS = `pkg-config x11 --cflags`
+X11LIBS  = `pkg-config x11 --libs`
 
 # Xinerama, comment if you don't want it
-XINERAMALIBS  = -lXinerama
-XINERAMAFLAGS = -DXINERAMA
-
-# freetype
-FREETYPELIBS = -lfontconfig -lXft
-FREETYPEINC = /usr/include/freetype2
-
-# alpha
-XRENDERLIBS = -lXrender
-
-# OpenBSD (uncomment)
-#FREETYPEINC = $(X11INC)/freetype2
+XINERAMAFLAGS = `pkg-config xinerama && pkg-config xinerama --cflags` `pkg-config xinerama && echo "-DXINERAMA"`
+XINERAMALIBS  = `pkg-config xinerama && pkg-config xinerama --libs`
 
 # includes and libs
-INCS = -I$(X11INC) -I$(FREETYPEINC)
-LIBS = -L$(X11LIB) -lX11 $(XINERAMALIBS) $(FREETYPELIBS) ${XRENDERLIBS}
+LIBFLAGS = $(XINERAMAFLAGS) `pkg-config $(REQ_LIBS) --cflags`
+LIBS     = $(XINERAMALIBS) `pkg-config $(REQ_LIBS) --libs`
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"$(VERSION)\" $(XINERAMAFLAGS)
-CFLAGS   = -std=c99 -pedantic -Wall -Os $(INCS) $(CPPFLAGS)
+CFLAGS   = -std=c99 -pedantic -Wall -Os $(LIBFLAGS) $(CPPFLAGS)
 LDFLAGS  = $(LIBS)
 
 # compiler and linker
